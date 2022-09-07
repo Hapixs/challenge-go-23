131f555dbba398879664943c07969eea341c612d
Hapixs <alexs2341@gmail.com>
2022-09-07 12:57:16 +0200

    Add comb2

---
+++ printcomb2.go
@@ -0,0 +1,34 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	i := 0
+	j := 0
+
+	k := 0
+	l := 0
+	for ref := 0; ref < 9; ref++ {
+		for sref := 0; sref <= 9; sref++ {
+			for tref := 0; tref < 9; tref++ {
+				j = sref
+				if tref+1 <= sref {
+					continue
+				}
+				l = tref + 1
+				z01.PrintRune(rune(i + 48))
+				z01.PrintRune(rune(j + 48))
+				z01.PrintRune(' ')
+				z01.PrintRune(rune(k + 48))
+				z01.PrintRune(rune(l + 48))
+				if !(i == 8 && j == 8 && k == 9 && l == 9) {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+			}
+			i = ref
+			k = ref + 1
+		}
+	}
+	z01.PrintRune('\n')
+}
