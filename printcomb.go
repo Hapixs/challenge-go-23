c40df2f8b074501d24e815d2213c50b7e2ef4e0a
Hapixs <alexs2341@gmail.com>
2022-09-07 11:54:59 +0200

    parch

--- printcomb.go
+++ printcomb.go
@@ -4,19 +4,14 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for first := 0; first < 9; first++ {
-
 		for second := 0; second < 9; second++ {
-
 			for second <= first && first != 9 {
 				second++
 			}
-
 			for third := 0; third < 9; third++ {
-
-				for third <= second && first != 9 {
+				for third <= second && first != 9 && second != 9 {
 					third++
 				}
-
 				z01.PrintRune(rune(first + 48))
 				z01.PrintRune(rune(second + 48))
 				z01.PrintRune(rune(third + 48))
@@ -24,8 +19,6 @@ func PrintComb() {
 				z01.PrintRune(' ')
 
 			}
-
 		}
 	}
-
 }
