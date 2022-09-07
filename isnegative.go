075192dcce0aefae05117ad91367251018d11168
Hapixs <alexs2341@gmail.com>
2022-09-07 11:15:38 +0200

    change isNegative() to IsNegative()

--- isnegative.go
+++ isnegative.go
@@ -3,12 +3,12 @@ package student
 import "github.com/01-edu/z01"
 
 func main() {
-	isNegative(1)
-	isNegative(0)
-	isNegative(-1)
+	IsNegative(1)
+	IsNegative(0)
+	IsNegative(-1)
 }
 
-func isNegative(nb int) {
+func IsNegative(nb int) {
 	z01.PrintRune(map[bool]rune{true: rune(84), false: rune(70)}[nb < 0])
 	z01.PrintRune('\n')
 }
