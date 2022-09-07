37cde5206e90de5f50138bb21d6e3e490b1c68e0
Hapixs <alexs2341@gmail.com>
2022-09-07 13:23:12 +0200

    fix

--- printcomb2.go
+++ printcomb2.go
@@ -31,7 +31,10 @@ func PrintComb2() {
 	for i := 0; i <= 9; i++ {
 		for j := 0; j <= 9; j++ {
 			for k := i; k <= 9; k++ {
-				for l := j + 1; l <= 9; l++ {
+				for l := j; l <= 9; l++ {
+					if i == k && j == l {
+						continue
+					}
 					z01.PrintRune(rune(i + 48))
 					z01.PrintRune(rune(j + 48))
 					z01.PrintRune(' ')
