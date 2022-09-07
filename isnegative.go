f281a3c8e53534bcca7c4e85123f254cf1c224b4
Hapixs <alexs2341@gmail.com>
2022-09-07 11:12:36 +0200

    add isnegative

---
+++ isnegative.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	isNegative(1)
+	isNegative(0)
+	isNegative(-1)
+}
+
+func isNegative(nb int) {
+	z01.PrintRune(map[bool]rune{true: rune(84), false: rune(70)}[nb < 0])
+	z01.PrintRune('\n')
+}
