81d43a72bd526618a49ee40bfd1548cf1183d91b
Hapixs <alexs2341@gmail.com>
2022-09-07 13:34:18 +0200

    fix

--- printcomb2.go
+++ printcomb2.go
@@ -31,7 +31,7 @@ func PrintComb2() {
 	for i := 0; i <= 9; i++ {
 		for j := 0; j < 9; j++ {
 			for k := i; k <= 9; k++ {
-				for l := 0; l <= 9; l++ {
+				for l := j; l <= 9; l++ {
 					if i == k && j == l {
 						continue
 					}
