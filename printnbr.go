289f55ca52ecd1792d683e16b45650c99a15f0b1
Hapixs <alexs2341@gmail.com>
2022-09-07 16:38:46 +0200

    add printnbr

--- printnbr.go
+++ printnbr.go
@@ -1,14 +1,30 @@
 package piscine
 
-import (
-	"strconv"
-
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-	s1 := strconv.Itoa(n)
-	for _, ch := range s1 {
-		z01.PrintRune(ch)
+	negative := n < 0
+	if negative {
+		n *= -1
+	}
+
+	if negative {
+		z01.PrintRune('-')
+	}
+
+	rec(n)
+}
+
+func rec(n int) {
+	for i := 0; i <= 9; i++ {
+		if n <= 9 {
+			z01.PrintRune(rune(n + 48))
+			return
+		}
+		if (n-i)%10 == 0 {
+			rec((n - i) / 10)
+			z01.PrintRune(rune(i + 48))
+			return
+		}
 	}
 }
