8c495d67f787e71a031aa22155609bfb4ad59355
Hapixs <alexs2341@gmail.com>
2022-09-07 13:42:42 +0200

    finalize

--- printcomb2.go
+++ printcomb2.go
@@ -3,31 +3,6 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	/*for ref := 0; ref <= 9; ref++ { // I J K L
-		i = ref
-		if ref != 9 {
-			k = ref + 1
-		}
-		for sref := 0; sref <= 9; sref++ {
-			for tref := 0; tref < 9; tref++ {
-				j = sref
-				if tref+1 < sref || tref+1 == sref {
-					continue
-				}
-				l = tref + 1
-				z01.PrintRune(rune(i + 48))
-				z01.PrintRune(rune(j + 48))
-				z01.PrintRune(' ')
-				z01.PrintRune(rune(k + 48))
-				z01.PrintRune(rune(l + 48))
-				if !(i == 8 && j == 9 && k == 9 && l == 9) {
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
-				}
-			}
-		}
-	}*/
-
 	for i := 0; i <= 9; i++ {
 		for j := 0; j <= 9; j++ {
 			for k := i; k <= 9; k++ {
@@ -40,10 +15,13 @@ func PrintComb2() {
 					z01.PrintRune(' ')
 					z01.PrintRune(rune(k + 48))
 					z01.PrintRune(rune(l + 48))
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
+					if !(i == 9 && j == 8 && k == 9 && l == 9) {
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
 				}
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
