c2523304a88bd85dac7bb44c690337f4cc802bab
Hapixs <alexs2341@gmail.com>
2022-09-07 10:58:02 +0200

    removed print func

--- printdigits/main.go
+++ printdigits/main.go
@@ -1,10 +1,12 @@
 package main
 
+import "github.com/01-edu/z01"
+
 // simple function
 // reverse print alaphabet using ASCII ref to optimize
 func main() {
-	for i := 0; i <= 9; i++ {
-		print(i)
+	for i := 48; i <= 57; i++ {
+		z01.PrintRune(rune(i))
 	}
-	print("\n")
+	z01.PrintRune('\n')
 }
