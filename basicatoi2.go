247426271e41b5a883ced05bcde8b815c5a88197
Hapixs <alexs2341@gmail.com>
2022-09-08 11:15:08 +0200

    fix

--- basicatoi2.go
+++ basicatoi2.go
@@ -5,10 +5,7 @@ func BasicAtoi2(s string) int {
 		return 0
 	}
 	str := ""
-	runes := []rune{}
-	for _, st := range s {
-		runes = append(runes, st)
-	}
+	runes := populateArray([]rune{}, s)
 
 	for i, c := range runes {
 		if i > 0 && c == runes[i-1] {
@@ -16,17 +13,29 @@ func BasicAtoi2(s string) int {
 		}
 		str += string(c)
 	}
-	s = ""
+	runes = populateArray([]rune{}, str)
+
 	total := 0
 	for i := 0; i < len(runes); i++ {
 		for j := 0; j <= 10; j++ {
 			if j == 10 {
+				if runes[i] == ' ' {
+					break
+				}
 				return 0
 			}
 			if string(runes[i]) == string(rune(j+48)) {
 				total = total*10 + j
+				break
 			}
 		}
 	}
 	return total
+}
+
+func populateArray(runes []rune, str string) []rune {
+	for _, st := range str {
+		runes = append(runes, st)
+	}
+	return runes
 }
