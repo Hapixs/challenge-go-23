802a832c181322457cbf4449f3c236ec270ca54e
Hapixs <alexs2341@gmail.com>
2022-09-08 12:48:23 +0200

    fix

--- sortintegertable.go
+++ sortintegertable.go
@@ -1,22 +1,23 @@
 package piscine
 
 func SortIntegerTable(table []int) {
-	m := make(map[int]int)
+	m := make(map[int][]int)
 	var maxValue int = table[0]
 	var minValue int = table[0]
 	for _, i := range table {
-		m[i] = i
+		m[i] = append(m[i], i)
 		if i > maxValue {
 			maxValue = i
-		}
-		if i < minValue {
+		} else if i < minValue {
 			minValue = i
 		}
 	}
 	tb := []int{}
 	for i := minValue; i <= maxValue; i++ {
 		if val, ok := m[i]; ok {
-			tb = append(tb, val)
+			for _, v := range val {
+				tb = append(tb, v)
+			}
 		}
 	}
 	copy(table, tb)
