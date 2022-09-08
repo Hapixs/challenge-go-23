e5a81290bb8beef664091afee7527d6e4eceee12
Hapixs <alexs2341@gmail.com>
2022-09-08 09:05:21 +0200

    Whouaaaow nice readme man

---
+++ README.md
@@ -0,0 +1,1 @@
+![alt text](https://tenor.com/view/wtc-antoine-daniel-what-the-cut-what-the-cut-russie-wtc-russie-gif-24948103)
--- main/main.go
+++ main/main.go
@@ -5,7 +5,7 @@ import "piscine"
 func main() {
 	// piscine.PrintComb()
 	// piscine.PrintComb2()
-	piscine.PrintNbr(-9)
+	// piscine.PrintNbr(-9)
 
-	// piscine.PrintCombN(1)
+	piscine.PrintCombN(11)
 }
