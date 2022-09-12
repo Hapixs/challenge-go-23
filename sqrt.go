9b9ef2cf76d8993c88e415475e74dc1b0d4f8068
Hapixs <alexs2341@gmail.com>
2022-09-12 12:31:03 +0200

    add sqrt

--- sqrt.go
+++ sqrt.go
@@ -1,11 +1,8 @@
 package piscine
 
 func Sqrt(n int) int {
-	if n == 1 {
-		return 1
-	}
 	for i := 1; i < n; i++ {
-		if n/i == i && (n/i)%1.0 != 0 {
+		if n/i == i && (float64(n)/float64(i)) == float64(int64(n/i)) {
 			return i
 		}
 	}
