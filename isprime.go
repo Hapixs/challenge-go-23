eaa85c2961c1764418ef5090e36740c7b5795eeb
Hapixs <alexs2341@gmail.com>
2022-09-12 12:48:43 +0200

    add isprime

--- isprime.go
+++ isprime.go
@@ -4,8 +4,9 @@ func IsPrime(n int) bool {
 	if n <= 1 {
 		return false
 	}
-	for i := 1; i < n; i++ {
-		if (float64(n)/float64(i)) == float64(int64(n/i)) && n/i == i {
+	i := 1
+	for i = 2; i < n; i++ {
+		if (float64(n) / float64(i)) == float64(int64(n/i)) {
 			return false
 		}
 	}
