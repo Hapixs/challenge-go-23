b48d4955f4c0d948a5b03625b1e7224595d856b5
Hapixs <alexs2341@gmail.com>
2022-09-12 11:12:39 +0200

    add iterativePower

--- iterativepower.go
+++ iterativepower.go
@@ -1,13 +1,12 @@
 package piscine
 
 func IterativePower(nb, power int) int {
-	if nb < 0 {
+	if power < 0 {
 		return 1
 	}
 	fnl := nb
 	for i := 1; i < power; i++ {
 		fnl *= nb
 	}
-
 	return fnl
 }
