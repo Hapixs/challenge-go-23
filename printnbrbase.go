a1b7bc3fbe79a5c4f421f82c0ad7b5a06ffecd2c
Hapixs <alexs2341@gmail.com>
2022-09-15 12:10:29 +0200

    fix printnbrbase

--- printnbrbase.go
+++ printnbrbase.go
@@ -3,6 +3,16 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbrBase(nbr int, base string) {
+	for _, c := range base {
+		for _, c1 := range base {
+			if c == c1 {
+				z01.PrintRune('N')
+				z01.PrintRune('V')
+				return
+			}
+		}
+	}
+
 	negative := false
 	if nbr < 0 {
 		negative = true
