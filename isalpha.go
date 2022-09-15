eb9ecdc64d6fe85a9a33079dd3b6fb9803b5d2b4
Hapixs <alexs2341@gmail.com>
2022-09-15 09:59:05 +0200

    fix isAlpha

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, c := range s {
-		if (c < 97 && c > 122) && (c < 65 && c > 90) {
+		if !(c >= 97 && c <= 122) || !(c >= 65 && c <= 90) {
 			return false
 		}
 	}
