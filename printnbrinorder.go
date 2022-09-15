b497ac4163fd15b7faea31d257894a67abb99fff
Hapixs <alexs2341@gmail.com>
2022-09-15 11:25:03 +0200

    fix printnbrinorder

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -10,14 +10,16 @@ func PrintNbrInOrder(n int) {
 
 func defineTab(n int) []int {
 	tab := []int{}
-	for i := 0; i < 9; i++ {
-		if n <= 9 {
-			tab = append(tab, i)
-			return tab
-		}
-		if (n-i)%10 == 0 {
-			tab = append(tab, i)
-			return defineTab((n - i) / 10)
+	for n >= 0 {
+		for i := 0; i < 9; i++ {
+			if n <= 9 {
+				tab = append(tab, i)
+				n = 0
+			}
+			if (n-i)%10 == 0 {
+				tab = append(tab, i)
+				n = (n - i) % 10
+			}
 		}
 	}
 	return tab
