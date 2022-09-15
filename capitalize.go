32a29a190d1b8ef2d7f70014cb278b06fb5ca812
Hapixs <alexs2341@gmail.com>
2022-09-15 16:50:58 +0200

    flex optimization ;*

--- capitalize.go
+++ capitalize.go
@@ -6,7 +6,7 @@ func Capitalize(s string) string {
 		if i == 0 && IsAlpha(string(c)) {
 			r[i] = rune(ToUpper(string(c))[0])
 		} else if IsAlpha(string(r)) && !IsAlpha(string(r[i-1])) {
-			r[i-1] = rune(ToUpper(string(r[i-1]))[0])
+			r[i] = rune(ToUpper(string(r[i]))[0])
 		} else if !IsLower(string(c)) {
 			r[i] = rune(ToLower(string(c))[0])
 		}
