950365767b95a786fd0037b5de123fa3e7603c54
Hapixs <alexs2341@gmail.com>
2022-09-15 12:08:22 +0200

    add base

--- base.go
+++ base.go
@@ -1,11 +1,24 @@
 package piscine
 
+import "github.com/01-edu/z01"
+
 func Base(nbr int, base string) {
+	negative := false
+	if nbr < 0 {
+		negative = true
+		nbr = nbr * -1
+	}
 	nbase := len([]rune(base))
 	str := ""
 	for nbr/nbase > 0 {
 		str = string(base[nbr%nbase]) + str
 		nbr = nbr / nbase
 	}
-	println(string(base[nbr]) + str)
+	if negative {
+		z01.PrintRune('-')
+	}
+	z01.PrintRune(rune(base[nbr]))
+	for _, s := range str {
+		z01.PrintRune(s)
+	}
 }
