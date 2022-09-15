acd88a8c3990f5d661aad0b7d0ad7b429db87362
Hapixs <alexs2341@gmail.com>
2022-09-15 11:03:43 +0200

    fix join

---
+++ join.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Join(s []string, sep string) string {
+	if len(s) <= 0 {
+		return ""
+	}
+	str := s[0]
+	for i := 1; i <= len(s); i++ {
+		str += sep + s[i]
+	}
+	return str
+}
