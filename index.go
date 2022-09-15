a2119c943781c0cb362a4bbf7d59bb8ee9ff5f10
Hapixs <alexs2341@gmail.com>
2022-09-15 09:49:13 +0200

    fix index

--- index.go
+++ index.go
@@ -4,12 +4,17 @@ func Index(s, find string) int {
 	rs := []rune(s)
 	fs := []rune(find)
 
+	if len(fs) > len(rs) {
+		return -1
+	}
+
 	for i, c := range s {
 		if c == fs[0] {
 			for y, fc := range find {
 				if rs[i+y] != fc {
 					break
-				} else if y+1 == len(rs) {
+				}
+				if y+1 == len(fs) {
 					return i
 				}
 			}
