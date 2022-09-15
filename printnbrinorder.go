a7e43bd559f74ce79ff3ab75ed58ad3376a0856e
Hapixs <alexs2341@gmail.com>
2022-09-15 11:22:39 +0200

    fix printnbrinorder

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -10,15 +10,14 @@ func PrintNbrInOrder(n int) {
 
 func defineTab(n int) []int {
 	tab := []int{}
-	for i := int(0); i <= 9; i++ {
-		if n <= 9 && n >= -9 { // Si le nombre n est plus petit que 10 (ou plus grand que -10 si il est negatif) alors pas besoin de ce prendre la tête on à juste à l'afficher en rune depuis l'ASCII
-			tab = append(tab, n)
+	for i := 0; i < 9; i++ {
+		if n <= 9 {
+			tab = append(tab, i)
 			return tab
 		}
 		if (n-i)%10 == 0 {
-			tab = append(tab, n)
-			tab = append(tab, defineTab((n-i)/10)...) // On rebalance donc notre chiffre n dans la fonction de récursion en enlevant ce qu'on vient de trouver (i) et on le divise par 10 pour atteindre la prochaine unité à trouver
-			return tab
+			tab = append(tab, i)
+			return defineTab((n - i) / 10)
 		}
 	}
 	return tab
