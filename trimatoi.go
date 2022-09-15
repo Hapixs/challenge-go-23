0e8b7e6be06786063a37c65d9b0072e84e7b2509
Hapixs <alexs2341@gmail.com>
2022-09-15 11:41:56 +0200

    add trimatoi

---
+++ trimatoi.go
@@ -0,0 +1,17 @@
+package piscine
+
+func TrimAtoi(s string) int {
+	negative := false
+	total := 0
+	for _, c := range s {
+		if c >= 48 && c <= 57 {
+			total = total*10 + int(c-48)
+			continue
+		} else if c == '-' || c == '+' && total == 0 {
+			negative = c == '-'
+			continue
+		}
+	}
+
+	return map[bool]int{true: total * -1, false: total}[negative] // Ici on retourn le chiffre en positif si (negative == false) ou en negatif si (negative == true)
+}
