a7931482cb86f01a4c56d44ce9aa2c972bae709e
Hapixs <alexs2341@gmail.com>
2022-09-15 09:36:36 +0200

    fix compare

--- compare.go
+++ compare.go
@@ -1,17 +1,11 @@
 package piscine
 
 func Compare(a, b string) int {
-	ar := []rune(a)
-	br := []rune(b)
-
-	if len(ar) < len(br) {
-		return -1
-	}
-	if len(ar) > len(br) {
-		return 1
-	}
 	if a == b {
 		return 0
 	}
-	return 1
+	if a < b {
+		return -1
+	}
+	return +1
 }
