8b88893fcda903932fb26d27380dce22187bcbc6
Hapixs <alexs2341@gmail.com>
2022-09-16 11:42:36 +0200

    flex optimisation

--- alphacount.go
+++ alphacount.go
@@ -1,5 +1,8 @@
 package piscine
 
 func AlphaCount(s string) int {
+	if StrLen(s) < 1 {
+		return 0
+	}
 	return map[bool]int{true: map[bool]int{true: 1 + AlphaCount(string(s[1:])), false: 0}[IsAlpha(s)], false: 0}[StrLen(s) > 0]
 }
