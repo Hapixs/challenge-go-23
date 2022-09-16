a5d8b40813b9383cb3f6c25240ed85134fdde670
Hapixs <alexs2341@gmail.com>
2022-09-16 11:33:13 +0200

    flex optimisation

--- basicjoin.go
+++ basicjoin.go
@@ -1,8 +1,5 @@
 package piscine
 
 func BasicJoin(strings []string) string {
-	if len(strings) < 1 {
-		return ""
-	}
-	return strings[0] + map[bool]string{true: BasicJoin(strings[1:]), false: ""}[len(strings) > 1]
+	return Join(strings, "")
 }
