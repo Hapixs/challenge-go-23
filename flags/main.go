32011f80c319d05b69557c13875dad6cbe89df3d
Hapixs <alexs2341@gmail.com>
2022-09-16 16:53:27 +0200

    add flags

---
+++ flags/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) < 1 || args[0] == "--help" || args[0] == "-h" {
+		printHelp()
+		return
+	}
+
+	for index, s := range args {
+		i := StrIndex(s, "--insert=") + StrIndex(s, "-i=")
+		if i+2 > 0 {
+			toInsert := ""
+			vIndex := StrIndex(s, "=")
+			for _, c := range s[vIndex+1:] {
+				toInsert += string(c)
+			}
+			toInsert = args[index+1] + toInsert
+			fmt.Print(toInsert)
+			continue
+		}
+		if s == "-o" || s == "--order" {
+			toOrder := []rune{}
+			for _, c := range args[index+1] {
+				toOrder = append(toOrder, c)
+			}
+			SortRuneTable(toOrder)
+			for _, c := range toOrder {
+				z01.PrintRune(c)
+			}
+		}
+	}
+	z01.PrintRune('\n')
+
+}
+
+func printHelp() {
+	fmt.Println("--insert")
+	fmt.Println("  -i")
+	fmt.Println("     This flag inserts the string into the string passed as argument.")
+	fmt.Println("--order")
+	fmt.Println("  -o")
+	fmt.Println("     This flag will behave like a boolean, if it is called it will order the argument.")
+}
+
+func SortRuneTable(table []rune) {
+	for i := 0; i < len(table); i++ { // Boucle qui passe sur chaque valeur du tableau
+		for j := i; j > 0 && table[j-1] > table[j]; j-- { // Boucle qui repasse sur chaque valeur du tableau dans le sens inverse de i, tant que la valeur précedente de la liste (table[j-1]) est inferieur à celle actuel (table[j])
+			table[j], table[j-1] = table[j-1], table[j] // Echange des deux valeur si table[j-1] > table[j]
+		}
+	}
+}
+
+func StrIndex(s, find string) int {
+	SizeS := StrLen(s)
+	SizeF := StrLen(find)
+	for i := 0; i < SizeS-SizeF; i++ {
+		if s[i:i+SizeF] == find {
+			return i
+		}
+	}
+	return -1
+}
+
+func Index(s []string, find string) int {
+	SizeS := len(s)
+	SizeF := StrLen(find)
+	for index, str := range s {
+		for i := 0; i < SizeS-SizeF; i++ {
+			if str[i:i+SizeF] == find {
+				return index
+			}
+		}
+	}
+	return -1
+}
+
+func StrLen(s string) int {
+	return len([]rune(s))
+}
