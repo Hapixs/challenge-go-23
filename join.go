b76dac8ca4bf12e961abce818ea99a5269f44902
Hapixs <alexs2341@gmail.com>
2022-09-16 11:30:29 +0200

    flex optimisation

--- join.go
+++ join.go
@@ -1,5 +1,8 @@
 package piscine
 
 func Join(s []string, sep string) string {
-	return map[bool]string{false: s[0] + map[bool]string{true: sep, false: ""}[len(s) > 1] + Join(s[1:], sep), true: ""}[len(s) <= 0]
+	if len(s) <= 0 {
+		return ""
+	}
+	return s[0] + map[bool]string{true: sep, false: ""}[len(s) > 1] + Join(s[1:], sep)
 }
