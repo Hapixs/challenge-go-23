362a5215da6e05239a88a8feed31a915849e848e
Hapixs <alexs2341@gmail.com>
2022-09-16 15:15:27 +0200

    fix

--- sortparams/main.go
+++ sortparams/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	table := os.Args
+	table := os.Args[1:]
 	for i := 0; i < len(table); i++ { // Boucle qui passe sur chaque valeur du tableau
 		for j := i; j > 0 && table[j-1] > table[j]; j-- { // Boucle qui repasse sur chaque valeur du tableau dans le sens inverse de i, tant que la valeur précedente de la liste (table[j-1]) est inferieur à celle actuel (table[j])
 			table[j], table[j-1] = table[j-1], table[j] // Echange des deux valeur si table[j-1] > table[j]
