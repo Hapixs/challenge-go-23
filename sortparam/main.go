bb8cb60ac4841041e286c7e5c5adcb7dddaaeefd
Hapixs <alexs2341@gmail.com>
2022-09-16 15:13:31 +0200

    add sortparam

---
+++ sortparam/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	table := os.Args
+	for i := 0; i < len(table); i++ { // Boucle qui passe sur chaque valeur du tableau
+		for j := i; j > 0 && table[j-1] > table[j]; j-- { // Boucle qui repasse sur chaque valeur du tableau dans le sens inverse de i, tant que la valeur précedente de la liste (table[j-1]) est inferieur à celle actuel (table[j])
+			table[j], table[j-1] = table[j-1], table[j] // Echange des deux valeur si table[j-1] > table[j]
+		}
+	}
+	for _, c := range table {
+		for _, r := range c {
+			z01.PrintRune(r)
+		}
+		z01.PrintRune('\n')
+	}
+}
