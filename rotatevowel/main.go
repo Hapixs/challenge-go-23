617d270efe32b3cfd9b6a28dcff376f37960f78c
Hapixs <alexs2341@gmail.com>
2022-09-16 17:43:16 +0200

    add rotatevowel

---
+++ rotatevowel/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	str := Join(os.Args[1:], " ")
+	r := []rune(str)
+	for i := 0; i < len(r); i++ {
+		a := r[i]
+		if isVowel(a) {
+			for y := i; y < len(r); y++ {
+				b := r[y]
+				if isVowel(b) {
+					r[i], r[y] = r[y], r[i]
+				}
+			}
+		}
+	}
+	fmt.Println(string(r))
+}
+
+func Join(s []string, sep string) string {
+	if len(s) <= 0 {
+		return ""
+	}
+	return s[0] + map[bool]string{true: sep, false: ""}[len(s) > 1] + Join(s[1:], sep)
+}
+
+func isVowel(r rune) bool {
+	str := "aeiouAEIOU"
+	return StrIndex(str, string(r)) >= 0
+}
+
+func StrIndex(s, find string) int {
+	SizeS := StrLen(s)
+	SizeF := StrLen(find)
+	for i := 0; i < SizeS-SizeF; i++ {
+		if s[i:i+SizeF] == find {
+			return i
+		}
+	}
+	return -1
+}
+
+func StrLen(s string) int {
+	return len([]rune(s))
+}
