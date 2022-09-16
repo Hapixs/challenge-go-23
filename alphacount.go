22235f15cc90d3ba3ce0ad6595fb3e03964ca2ef
Hapixs <alexs2341@gmail.com>
2022-09-16 11:49:02 +0200

    flex optimisation

--- alphacount.go
+++ alphacount.go
@@ -4,5 +4,9 @@ func AlphaCount(s string) int {
 	if StrLen(s) < 1 {
 		return 0
 	}
-	return map[bool]int{true: map[bool]int{true: 1 + AlphaCount(string(s[1:])), false: 0}[IsPrintable(string(s[0]))], false: 0}[StrLen(s) > 0]
+	return map[bool]int{true: map[bool]int{true: 1 + AlphaCount(string(s[1:])), false: 0}[isLetter([]rune(s)[0])], false: 0}[StrLen(s) > 0]
+}
+
+func isLetter(c rune) bool {
+	return (c >= 97 && c <= 122) || (c >= 65 && c <= 90)
 }
