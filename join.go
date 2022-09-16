c82fd515fbe1313aca5bf95917b2e361a84150a1
Hapixs <alexs2341@gmail.com>
2022-09-16 11:07:16 +0200

    flex optimisation

--- join.go
+++ join.go
@@ -4,5 +4,5 @@ func Join(s []string, sep string) string {
 	if len(s) <= 0 {
 		return ""
 	}
-	return s[0] + sep + Join(s[1:], sep)
+	return s[0] + map[bool]string{true: sep, false: ""}[len(s) > 1] + Join(s[1:], sep)
 }
