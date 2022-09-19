ed411dba0615031bc8fd26ca8bed4bd096e18c73
Hapixs <alexs2341@gmail.com>
2022-09-19 15:29:07 +0200

    add split

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -5,8 +5,14 @@ func SplitWhiteSpaces(str string) []string {
 		if s == ' ' || s == '\n' || s == '\t' {
 			if len(str[:i]) > 0 {
 				return append([]string{str[:i]}, SplitWhiteSpaces(str[i+1:])...)
+			} else {
+				return append([]string{}, SplitWhiteSpaces(str[i+1:])...)
 			}
 		}
 	}
-	return []string{str}
+	return []string{str} /*
+
+		[]string{"MIX", "V,c6)^$os", "5-7o|im29BrG?", "CsNe6U<^1+w1", " 2]I5ch>#g,V1{", "/46UIg&RxuIe", " \\oOM)~8$Vcw]Z", "K0l6y\\JFTpP&V", "SIZ.-&0@}%i(\""} instead of
+		[]string{"MIX", "V,c6)^$os", "5-7o|im29BrG?", "CsNe6U<^1+w1", "2]I5ch>#g,V1{", "/46UIg&RxuIe", "\\oOM)~8$Vcw]Z", "K0l6y\\JFTpP&V", "SIZ.-&0@}%i(\""}
+	*/
 }
