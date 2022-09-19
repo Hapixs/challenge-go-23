c30fc93ab1658bbe7b9976cbc5f892ad69ddafa2
Hapixs <alexs2341@gmail.com>
2022-09-19 15:41:17 +0200

    add split

---
+++ split.go
@@ -0,0 +1,14 @@
+package piscine
+
+func Split(str, sep string) []string {
+
+	for len(str) > 0 {
+		i := Index(str, sep)
+		if i < 0 {
+			return []string{}
+		}
+		return append([]string{str[:i]}, Split(str[i+2:], sep)...)
+	}
+
+	return []string{}
+}
