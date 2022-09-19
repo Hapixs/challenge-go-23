9cad79174b20ea35d43ebd449be6a5f892de0c64
Hapixs <alexs2341@gmail.com>
2022-09-19 15:44:28 +0200

    add split

--- split.go
+++ split.go
@@ -6,7 +6,11 @@ func Split(str, sep string) []string {
 		if i < 0 {
 			return []string{str}
 		}
-		return append([]string{str[:i]}, Split(str[i+2:], sep)...)
+		return append([]string{str[:i]}, Split(str[i+len([]rune(sep)):], sep)...)
 	}
 	return []string{str}
+	/*
+		[]string{"xUnQLgHALgc1d", ">abc<>]UqmQFzHetoL4R", ">abc<>]R9oaFnlolSxfd", ">abc<>]y0JmAME3YK6VP", ">abc<>]PUUck2cdh24eG", ">abc<>]CkqoJyLfR1i0d", ">abc<>]TgRjXxVWqNPTZ", ">abc<>]bYd8nKWnrmVVg"} instead of
+		[]string{"xUnQLgHALgc1d", "UqmQFzHetoL4R", "R9oaFnlolSxfd", "y0JmAME3YK6VP", "PUUck2cdh24eG", "CkqoJyLfR1i0d", "TgRjXxVWqNPTZ", "bYd8nKWnrmVVg"}
+	*/
 }
