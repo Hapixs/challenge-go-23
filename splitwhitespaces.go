c1253864bdb7fa62697c8a257b05f0de3f46a042
Hapixs <alexs2341@gmail.com>
2022-09-19 15:27:41 +0200

    add split

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -3,7 +3,9 @@ package piscine
 func SplitWhiteSpaces(str string) []string {
 	for i, s := range str {
 		if s == ' ' || s == '\n' || s == '\t' {
-			return append([]string{str[:i]}, SplitWhiteSpaces(str[i+1:])...)
+			if len(str[:i]) > 0 {
+				return append([]string{str[:i]}, SplitWhiteSpaces(str[i+1:])...)
+			}
 		}
 	}
 	return []string{str}
