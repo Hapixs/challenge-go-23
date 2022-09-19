f0c01033015ecf2ac173f2a7a2d043fbfa5c2116
Hapixs <alexs2341@gmail.com>
2022-09-19 16:26:30 +0200

    add basetobase

--- convertbase.go
+++ convertbase.go
@@ -1,17 +1,17 @@
 package piscine
 
+func ToBase(nbr int, base string) string {
+	return ToBaseRec(nbr, []rune(base), len([]rune(base))-1)
+}
+
+func ToBaseRec(nbr int, baseTo []rune, size int) string {
+	if nbr < size {
+		return string(baseTo[nbr])
+	}
+	return ToBaseRec(nbr/size, baseTo, size) + string(baseTo[nbr%size])
+}
+
 func ConvertBase(nbr, fromBase, toBase string) string {
 	number := AtoiBase(nbr, fromBase)
 	return ToBase(number, toBase)
-}
-
-func ToBase(nbr int, base string) string {
-	return ToBaseRec(nbr, []rune(base))
-}
-
-func ToBaseRec(nbr int, baseTo []rune) string {
-	if nbr < len(baseTo)-1 {
-		return string(baseTo[nbr])
-	}
-	return ToBaseRec(nbr/(len([]rune(baseTo))-1), baseTo)
 }
