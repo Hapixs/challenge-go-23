9fd8349df5e08a8bf6903f99a0b64978d27e239e
Hapixs <alexs2341@gmail.com>
2022-09-19 16:27:57 +0200

    add basetobase

--- convertbase.go
+++ convertbase.go
@@ -1,14 +1,14 @@
 package piscine
 
-func ToBase(nbr int, base string) string {
-	return ToBaseRec(nbr, []rune(base), len([]rune(base))-1)
-}
-
 func ToBaseRec(nbr int, baseTo []rune, size int) string {
 	if nbr < size {
 		return string(baseTo[nbr])
 	}
 	return ToBaseRec(nbr/size, baseTo, size) + string(baseTo[nbr%size])
+}
+
+func ToBase(nbr int, base string) string {
+	return ToBaseRec(nbr, []rune(base), len([]rune(base)))
 }
 
 func ConvertBase(nbr, fromBase, toBase string) string {
