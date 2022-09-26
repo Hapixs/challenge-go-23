e3a656c544084c26b0ccbe347eed0cb4cf36bfe7
Hapixs <alexs2341@gmail.com>
2022-09-26 15:03:18 +0200

    add advencedsortwordarr

---
+++ advencedsortwordarr.go
@@ -0,0 +1,9 @@
+package piscine
+
+func AdvencedSortWordArr(a []string, f func(a, b string) int) {
+	for i := 0; i < len(a); i++ {
+		for j := i; j > 0 && f(a[j-1], a[j]) >= 1; j-- {
+			a[j-1], a[j] = a[j], a[j-1]
+		}
+	}
+}
