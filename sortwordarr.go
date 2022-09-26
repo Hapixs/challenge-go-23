2bfb0018216989da665ccc29dcfd67326fa1c621
Hapixs <alexs2341@gmail.com>
2022-09-26 14:42:14 +0200

    add sortwordarr

--- sortwordarr.go
+++ sortwordarr.go
@@ -1,12 +1,8 @@
 package piscine
 
 func SortWordArr(s []string) {
-	ru := []rune{}
-	for _, str := range s {
-		ru = append(ru, rune(str[0]))
-	}
-	for i := 0; i < len(ru); i++ {
-		for j := i + 1; j < len(ru) && rune(s[j-1][0]) > rune(s[j][0]); j++ {
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j < len(s) && rune(s[j-1][0]) < rune(s[j][0]); j++ {
 			s[j-1], s[j] = s[j], s[j-1]
 		}
 	}
