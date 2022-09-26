1ebbb505368aae4e941bc903c9ea4b2662893461
Hapixs <alexs2341@gmail.com>
2022-09-26 14:50:17 +0200

    add sortwordarr

--- sortwordarr.go
+++ sortwordarr.go
@@ -2,7 +2,7 @@ package piscine
 
 func SortWordArr(s []string) {
 	for i := 0; i < len(s); i++ {
-		for j := i; j > 0 && rune(s[j-1][0]) >= rune(s[j][0]); j-- {
+		for j := i; j > 0 && rune(s[j-1][0]) > rune(s[j][0]); j-- {
 			s[j-1], s[j] = s[j], s[j-1]
 		}
 	}
@@ -12,4 +12,7 @@ func SortWordArr(s []string) {
 [3 2j9xRyVdyUe j9 ctpX bwwgs QpCG nlFDZwK5 iUuZ532NG 3Hm 8owmxMG 47V   V7O89 n5LsL V Iy NWc2ecd je V pFH K UKkLl]
 [3 2j9xRyVdyUe 8owmxMG 47V   Iy NWc2ecd je QpCG nlFDZwK5 V7O89 n5LsL V V pFH K UKkLl iUuZ532NG 3Hm j9 ctpX bwwgs]
 [3 2j9xRyVdyUe 8owmxMG 47V   Iy NWc2ecd je QpCG nlFDZwK5 V pFH K UKkLl V7O89 n5LsL V iUuZ532NG 3Hm j9 ctpX bwwgs]
+
+[ HlNj8yy00Cdp 7Ibkunql  Kkm 8 6sd PcZpfx3 HkjbLGnw5n NG MBqFmM9 V2CBP glw3lNkQ fzss g5URlYJ8oCZ5G lbLEZV6npYp5L]
+[ HlNj8yy00Cdp 7Ibkunql  Kkm 8 6sd PcZpfx3 HkjbLGnw5n NG MBqFmM9 V2CBP g5URlYJ8oCZ5G glw3lNkQ fzss lbLEZV6npYp5L]
 */
