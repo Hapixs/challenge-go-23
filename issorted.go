7d3f6015de1d0ab853103c381bbd70240336c9aa
Hapixs <alexs2341@gmail.com>
2022-09-26 13:55:34 +0200

    add issorted

--- issorted.go
+++ issorted.go
@@ -2,7 +2,9 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) < 0 {
+		if f(a[i], a[i+1]) < 0 && a[0] > a[len(a)-1] {
+			return false
+		} else if f(a[i], a[i+1]) > 0 && a[0] < a[len(a-1)] {
 			return false
 		}
 	}
