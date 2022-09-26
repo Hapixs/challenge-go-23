2bf8a390b5f4e6ad97295a09e02301ba8f2ddecb
Hapixs <alexs2341@gmail.com>
2022-09-26 11:47:05 +0200

    add readfile

---
+++ displayfiles/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Println("File name missing")
+		return
+	} else if len(args) > 1 {
+		fmt.Println("Too many arguments")
+		return
+	}
+
+	content, err := ioutil.ReadFile("thermopylae.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(content))
+}
