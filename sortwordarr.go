cc6ed8acb15e44db5e0328e5be352c42f46a08c0
Hapixs <alexs2341@gmail.com>
2022-09-26 14:48:16 +0200

    add sortwordarr

--- sortwordarr.go
+++ sortwordarr.go
@@ -2,8 +2,14 @@ package piscine
 
 func SortWordArr(s []string) {
 	for i := 0; i < len(s); i++ {
-		for j := i; j > 0 && rune(s[j-1][0]) > rune(s[j][0]); j-- {
+		for j := i; j > 0 && rune(s[j-1][0]) >= rune(s[j][0]); j-- {
 			s[j-1], s[j] = s[j], s[j-1]
 		}
 	}
 }
+
+/*
+[3 2j9xRyVdyUe j9 ctpX bwwgs QpCG nlFDZwK5 iUuZ532NG 3Hm 8owmxMG 47V   V7O89 n5LsL V Iy NWc2ecd je V pFH K UKkLl]
+[3 2j9xRyVdyUe 8owmxMG 47V   Iy NWc2ecd je QpCG nlFDZwK5 V7O89 n5LsL V V pFH K UKkLl iUuZ532NG 3Hm j9 ctpX bwwgs]
+[3 2j9xRyVdyUe 8owmxMG 47V   Iy NWc2ecd je QpCG nlFDZwK5 V pFH K UKkLl V7O89 n5LsL V iUuZ532NG 3Hm j9 ctpX bwwgs]
+*/
