4cdf279eefc9a701b16cfa37833922e11cbe1ede
Hapixs <alexs2341@gmail.com>
2022-09-26 16:46:37 +0200

    add ztail

--- ztail/main.go
+++ ztail/main.go
@@ -25,7 +25,7 @@ func main() {
 		}
 		ct := string(content)
 		os.Stdout.WriteString("\n==> " + a + " <==\n")
-		if len(ct) <= c {
+		if len(ct) <= c+1 {
 			os.Stdout.WriteString(ct)
 			osExit = true
 			continue
