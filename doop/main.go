5e40ec024a45ee4327f3c5cf34fc788d6959708e
salexandre <alexandre.sarouille@ynov.com>
2022-09-27 14:46:55 +0200

    bouritos flex

--- doop/main.go
+++ doop/main.go
@@ -6,15 +6,13 @@ import (
 
 func main() {
 	args := os.Args[1:]
-	a1 := 0
-	a2 := 0
 
 	if len(args) != 3 || (!IsNumeric(args[0]) && IsNumeric(args[2])) {
 		return
 	}
 
-	a1 = Atoi(args[0])
-	a2 = Atoi(args[2])
+	a1 := Atoi(args[0])
+	a2 := Atoi(args[2])
 
 	if a1 >= 9223372036800000000 || a2 >= 9223372036800000000 {
 		return
@@ -51,20 +49,19 @@ func IsNumeric(s string) bool {
 }
 
 func Atoi(s string) int {
-	negative := false     // Boolean permettant de savoir si le chiffre est negatif (modifier plus tard)
-	total := 0            // chiffre total à retourner
-	for _, c := range s { // Boucle qui passe sur chaques rune du string d'entrer
-		if c >= 48 && c <= 57 { // Si le caractère actuel est bien un chiffre (ref ASCII)
-			total = total*10 + int(c-48) // On multipli le chiffre par 10 pour pour ajouter la nouvelle unité trouvée
-			continue                     // On passe le reste et on continue la boucle à l'iteration suivante
-		} else if c == '-' || c == '+' && total == 0 { // si la rune est un - ou + et qui le total n'as pas encore été modifier (sinon cela veux dire qu'on est au milieu du string)
-			negative = c == '-' // si la rune et - alors le chiffre et negatif sinon il est positif
-			continue            // on continue à l'iteration suivante de la boucle
+	negative := false     
+	total := 0             
+	for _, c := range s { 
+		if c >= 48 && c <= 57 { 
+			total = total*10 + int(c-48) 
+			continue                     
+		} else if c == '-' || c == '+' && total == 0 { 
+			negative = c == '-' 
+			continue 
 		}
-		return 0 // Tout autre cas, on retourne 0, le string n'est pas un chiffre valide pour l'algorithme
+		return 0
 	}
-	//     dictionaire [  <clé> : <valeur>, ...       ] la valeur que l'on cherche en fonction de la clé (negative donc soit true soit false dans ce cas)
-	return map[bool]int{true: total * -1, false: total}[negative] // Ici on retourn le chiffre en positif si (negative == false) ou en negatif si (negative == true)
+	return map[bool]int{true: total * -1, false: total}[negative] 
 }
 
 func Itoa(i int) string {
@@ -78,12 +75,9 @@ func Itoa(i int) string {
 		if i <= 9 && i > 0 {
 			return string(rune(i+48)) + str
 		}
-		for y := 0; y <= 9; y++ {
-			if (i-y)%10 == 0 {
-				i = (i - y) / 10
-				str = string(rune(y+48)) + str
-			}
-		}
+		y = i%10
+		i = (i - y) / 10
+		str = string(rune(y+48)) + str
 	}
 	return map[bool]string{true: "-" + str, false: str}[negative]
 }
