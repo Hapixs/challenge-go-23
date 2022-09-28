fb59d961d4dd21692686b9a61e8de4a3b803ca24
salexandre <alexandre.sarouille@ynov.com>
2022-09-28 15:14:30 +0200

    add sortinsert

--- sortlistinsert.go
+++ sortlistinsert.go
@@ -1,10 +1,11 @@
 package piscine
 
-func SortListInsert(nodeI *NodeI, ref int) {
+func SortListInsert(nodeI *NodeI, ref int) *NodeI {
 	it := nodeI
 	for it.Next != nil {
 		it = it.Next
 	}
 	it.Next = &NodeI{Data: ref}
 	ListSort(nodeI)
+	return nodeI
 }
