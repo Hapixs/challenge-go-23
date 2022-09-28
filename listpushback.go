4cc1d674f8137c7b06083fbf8cb4c0a06364c9e6
salexandre <alexandre.sarouille@ynov.com>
2022-09-28 14:08:49 +0200

    add push

---
+++ listpushback.go
@@ -0,0 +1,25 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	l.Head.Next = l.Tail
+	current := l.Head.Next
+	for {
+		if current.Next == nil {
+			current.Next = l.Tail
+			break
+		} else {
+			current = current.Next
+		}
+	}
+	l.Tail = &NodeL{data, &NodeL{}}
+}
