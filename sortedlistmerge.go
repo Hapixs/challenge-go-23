59ed5e2456523210ebef068875bd4ecd460e1510
salexandre <alexandre.sarouille@ynov.com>
2022-09-28 15:42:12 +0200

    add sortmerge

--- sortedlistmerge.go
+++ sortedlistmerge.go
@@ -1,6 +1,12 @@
 package piscine
 
 func SortedListMerge(n1 *NodeI, n2 *NodeI) *NodeI {
+	if n1 == nil {
+		return n2
+	}
+	if n2 == nil {
+		return n1
+	}
 	it := n1
 	for it.Next != nil {
 		it = it.Next
