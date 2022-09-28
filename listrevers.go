57bc7740e810c6b64aad8d45e31d0a101e93de65
salexandre <alexandre.sarouille@ynov.com>
2022-09-28 14:45:34 +0200

    add revers

---
+++ listrevers.go
@@ -0,0 +1,13 @@
+package piscine
+
+func ListRevers(l *List) *List {
+	list := &List{}
+	it := l.Head
+	if it == nil {
+		return nil
+	}
+	for it.Next != nil {
+		ListPushFront(list, it.Data)
+	}
+	return l
+}
