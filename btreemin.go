903a2c41c31212ae1207b960fbb2b05e66f4f1e7
salexandre <alexandre.sarouille@ynov.com>
2022-09-29 15:23:36 +0200

    add btreemin

--- btreemin.go
+++ btreemin.go
@@ -2,13 +2,10 @@ package piscine
 
 func BTreeMin(root *TreeNode) *TreeNode {
 	if root == nil {
+		return nil
+	}
+	if root.Left == nil {
 		return root
 	}
-	if root.Data > root.Left.Data {
-		return BTreeMin(root.Left)
-	}
-	if root.Data > root.Right.Data {
-		return BTreeMin(root.Right)
-	}
-	return root
+	return BTreeMin(root.Left)
 }
