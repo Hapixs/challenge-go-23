51f53bdf7b15e1e0942b7f24d069e8b1ada9aa29
salexandre <alexandre.sarouille@ynov.com>
2022-09-29 14:45:13 +0200

    add btreeapplybylevel

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -4,11 +4,9 @@ func BTreeApplyByLevel(root *TreeNode, f func(...interface{})) (int, error) {
 	if root == nil {
 		return 1, nil
 	}
-	f(root.Data)
 	right, left := root.Left, root.Right
-
 	if left != nil {
-		f(left)
+		f(left.Data)
 		BTreeApplyByLevel(left, f)
 	}
 	if right != nil {
