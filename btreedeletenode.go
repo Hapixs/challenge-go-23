07f90f4fd0ee29c0b1db5e9f2f2f111976cb8c59
salexandre <alexandre.sarouille@ynov.com>
2022-09-29 15:40:02 +0200

    add btreedelete

--- btreedeletenode.go
+++ btreedeletenode.go
@@ -2,7 +2,7 @@ package piscine
 
 func BTreeDeleteNode(root, node *TreeNode) *TreeNode {
 	if node.Parent == nil {
-		*node = *root
+		*node, *node.Right = *root.Left, *root.Right
 	} else {
 		*node = *node.Parent
 	}
