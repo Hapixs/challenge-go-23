8b2aea15415d5bf36b4174d4a86b3df6eb35808c
salexandre <alexandre.sarouille@ynov.com>
2022-09-29 14:30:32 +0200

    add btreelevelcount

--- btreelevelcount.go
+++ btreelevelcount.go
@@ -1,15 +1,18 @@
 package piscine
 
 func BTreeLevelCount(root *TreeNode) int {
-	count := 1
+	count := 0
 	if root == nil {
 		return 0
 	}
 	if root.Left != nil {
-		count += BTreeLevelCount(root.Left)
+		count += 1 + BTreeLevelCount(root.Left)
 	}
 	if root.Right != nil {
-		count += BTreeLevelCount(root.Right)
+		count += 1 + BTreeLevelCount(root.Right)
+	}
+	if count > 2 {
+		count--
 	}
 	return count
 }
