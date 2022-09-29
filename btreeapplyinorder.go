f577cb87d76c943383856f15a7dd81ce7e36a0e4
salexandre <alexandre.sarouille@ynov.com>
2022-09-29 14:13:54 +0200

    add btreeapplyinorder

--- btreeapplyinorder.go
+++ btreeapplyinorder.go
@@ -4,11 +4,11 @@ func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	f(root.Data)
 	if root.Left != nil {
 		BTreeApplyInorder(root.Left, f)
 	}
 	if root.Right != nil {
 		BTreeApplyInorder(root.Right, f)
 	}
+	f(root.Data)
 }
