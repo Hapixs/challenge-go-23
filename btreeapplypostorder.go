c49445840f4fc35cd15c291df09e2ba3ac57153f
salexandre <alexandre.sarouille@ynov.com>
2022-09-29 14:17:49 +0200

    add btreeapplypostorder

---
+++ btreeapplypostorder.go
@@ -0,0 +1,14 @@
+package piscine
+
+func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+	if root.Left != nil {
+		BTreeApplyInorder(root.Left, f)
+	}
+	if root.Right != nil {
+		BTreeApplyInorder(root.Right, f)
+	}
+	f(root.Data)
+}
