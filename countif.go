17f286db7c98b308271c35fc771593e52e26a9be
salexandre <alexandre.sarouille@ynov.com>
2022-09-30 13:28:46 +0200

    bouritos optimizations

--- countif.go
+++ countif.go
@@ -3,9 +3,7 @@ package piscine
 func CountIf(f func(string) bool, a []string) int {
 	am := 0
 	for _, s := range a {
-		if f(s) {
-			am++
-		}
+		am = map[bool]int{true: am + 1, false: am}[f(s)]
 	}
 	return am
 }
